14096f9586f9cf843aa4fd1ec5a241fd509a43f5
straws <NVetock1@gmail.com>
2025-05-24 20:49:35 -0500

    The bird has been created flappy.

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,25 +1,33 @@
-extends CharacterBody2D
+extends RigidBody2D
 
+var player_sprite_paths: Array[String] = [
+	"res://resources/bird_1.png",
+	"res://resources/bird_2.png"
+]
+var player_sprites: Array[Texture2D] = []
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var flap_strength: int = 100
 
+func _ready() -> void:
+	for path in player_sprite_paths:
+		var o = load(path) as Texture2D
+		player_sprites.append(o)
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _process(delta: float) -> void:
+	if get_input():
+		$AudioWingFlap.play()
+		linear_velocity = flap()
+	
+	if linear_velocity.y > 0.0:
+		$Sprite2D.texture = player_sprites[1]
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$Sprite2D.texture = player_sprites[0]
 
-	move_and_slide()
+
+
+func get_input() -> bool:
+	return Input.is_action_just_pressed("flap")
+
+## Returns upward velocity
+func flap() -> Vector2:
+	return Vector2.UP * flap_strength
